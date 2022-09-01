e6b2e3ed178bb1f3e13833f5b6d82509537e04ce
codefolk <alizuhaib828@gmail.com>
2022-09-01 22:41:47 +0500

    created a new repository

---
+++ index.js
@@ -0,0 +1,15 @@
+const dropdown = document.querySelector(".navigation-links"); 
+const crossButton = document.querySelector("#close-dropdown"); 
+const navButton = document.querySelector("#open-dropdown");
+const btn = document.querySelector(".navbar-toggle");
+
+navButton.addEventListener("click", () =>{
+    dropdown.style.display = "flex";
+    crossButton.style.display = "block";
+    navButton.style.display = "none";
+})
+crossButton.addEventListener("click", () => {
+    dropdown.style.display = "none";
+    crossButton.style.display = "none";
+    navButton.style.display = "block";
+})
