097f0ebeb56c56e55240f57f7764b6567fdf027f
codefolk <alizuhaib828@gmail.com>
2022-09-03 22:37:26 +0500

    added some improvements

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const dropdown = document.querySelector(".navigation-links"); 
-const crossButton = document.querySelector("#close-dropdown"); 
+const dropdown = document.querySelector(".navigation-links");
+const crossButton = document.querySelector("#close-dropdown");
 const navButton = document.querySelector("#open-dropdown");
 const btn = document.querySelector(".navbar-toggle");
 
-navButton.addEventListener("click", () =>{
+navButton.addEventListener("click", () => {
     dropdown.style.display = "flex";
     crossButton.style.display = "block";
     navButton.style.display = "none";
-})
+});
 crossButton.addEventListener("click", () => {
     dropdown.style.display = "none";
     crossButton.style.display = "none";
     navButton.style.display = "block";
-})
+  }
+);
+
